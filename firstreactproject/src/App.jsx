469a05b0f1c9fc66916bca1ab814d726a19c14f9
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-08-20 16:42:48 +0300

    Update App.jsx

--- firstreactproject/src/App.jsx
+++ firstreactproject/src/App.jsx
@@ -2,33 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './Login'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleClick = () => {
+    setMessage('Hoş Geldin Mehmet!');
+  };
+  const [message, setMessage] = useState('');
+ return (
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+<div>
+  <div>
+  <p>Kullanıcı adı giriniz:</p>
+  <input type='text'/>
+    </div> 
+
+    <div>
+  <p>Şifre giriniz:</p>
+  <input type='text'/>
+    </div>
+
+    <button onClick={handleClick}>Giriş Yap </button>
+    {message && <p>{message}</p>}
+    <div>
+  </div>
+
+ </div>
+
   )
 }
 
