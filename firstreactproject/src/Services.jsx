537a06e7fc1346f7d0546b12a27ef67f1b5e15d9
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-08-26 02:44:10 +0300

    Add files via upload

---
+++ firstreactproject/src/Services.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export const Services = () => {
+  return (
+        <div>
+            
+        <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Soluta ab quod in vero, non enim modi ut quidem officiis libero aspernatur
+         quisquam vitae esse optio animi molestias dolores doloremque dicta labore quae magni cumque.
+          Tempora ab fuga quo id. Vel cum magni assumenda?
+           Esse deserunt sint consequatur soluta placeat est laborum maiores officiis.
+            Aliquid illo, consequatur error distinctio doloremque consequuntur consectetur esse saepe voluptas aperiam alias 
+            recusandae autem totam non provident possimus soluta ipsam minima et quas quia ducimus? Rem molestias enim sit deleniti
+             aliquam sequi repellendus dolor tenetur a! Repudiandae commodi ducimus voluptate sequi. Voluptates repellat hic quibusdam 
+             libero.
+        </div>
+        <br/>
+        <div/>
+
+        <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Soluta ab quod in vero, non enim modi ut quidem officiis libero
+          aspernatur quisquam vitae esse optio animi molestias dolores
+           doloremque dicta labore quae magni cumque. Tempora ab fuga quo id
+            Vel cum magni assumenda? Esse deserunt sint consequatur soluta
+             placeat est laborum maiores officiis. Aliquid illo, consequatur error distinctio doloremque consequuntur 
+             consectetur esse saepe voluptas aperiam alias recusandae autem totam non provident possimus soluta ipsam minima
+              et quas quia ducimus? Rem molestias enim sit deleniti aliquam sequi repellendus dolor tenetur a! Repudiandae 
+              commodi ducimus voluptate sequi. Voluptates repellat hic quibusdam libero.
+
+        </div>
+        <br/>
+        </div>
+       
+  );
+};
