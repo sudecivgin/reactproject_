e3a2b63c5b6c8e514925d03816998845edd0c5a1
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-08-26 22:57:35 +0300

    Update Navbar.jsx

--- firstreactproject/src/Navbar.jsx
+++ firstreactproject/src/Navbar.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import './Navbar.css'
 import logo from '../assets/logo9.png';
+import { NavLink } from 'react-router-dom';
+import { IoMdSearch } from "react-icons/io";
+
 
 
 
 const Navbar =()=> {
+  const [click,setClick]=useState(false)
+  
   return (
     <div className='Navbar'>
         <img src ={logo} alt=''className='logo9' />
-      
-        
-        <ul>
-                <li>Ana Sayfa </li>
-<li>Hakkımızda</li>
-<li>Menü</li>
-<li>İletişim</li>
+
+      <ul className ='nav-menu'>          
+        <li>
+                  <NavLink className ="nav-link" to="/Ana Sayfa">Ana Sayfa</NavLink>
+
+</li>
+<li>
+<NavLink className ="nav-link" to="/Hakkımızda">Hakkımızda</NavLink>
+ 
+</li>
+<li>
+<NavLink className ="nav-link" to="/Menü">Menü</NavLink>
+ 
+</li>
+<li>
+
+<NavLink className ="nav-link" to="/İletişim">İletişim</NavLink>
+</li>
 
         </ul>
         <div className='search-box'>
             <input type='text' placeholder='Search'/>
+            <IoMdSearch />
+
             
         </div>
         
@@ -28,3 +46,6 @@ const Navbar =()=> {
 }
 
 export default Navbar
+      
+      
+        
