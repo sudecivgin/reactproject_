6647aaa31681c419838eda9225f5037d1afd384f
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-08-26 13:52:07 +0300

    Update App.jsx

--- firstreactproject/src/App.jsx
+++ firstreactproject/src/App.jsx
@@ -1,34 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './Login'
+import React from 'react'
+import Navbar from './components/Navbar'
 
-function App() {
-  const handleClick = () => {
-    setMessage('Hoş Geldin Mehmet!');
-  };
-  const [message, setMessage] = useState('');
- return (
-
-<div>
-  <div>
-  <p>Kullanıcı adı giriniz:</p>
-  <input type='text'/>
-    </div> 
-
-    <div>
-  <p>Şifre giriniz:</p>
-  <input type='text'/>
-    </div>
-
-    <button onClick={handleClick}>Giriş Yap </button>
-    {message && <p>{message}</p>}
-    <div>
-  </div>
-
- </div>
-
+const App = () => {
+  return (
+      <div className='container'>
+      <Navbar/>
+      </div>
   )
 }
 
