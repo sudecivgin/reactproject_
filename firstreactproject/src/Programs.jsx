51d86f6c5ffbfadc9c905bfc66236fa3d5ed8bae
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-08-28 18:34:53 +0300

    Add files via upload

---
+++ firstreactproject/src/Programs.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Programs.css'
+
+const Programs = () => {
+  return (
+    <div className='programs'>
+        <div className="programs">
+            <div className='programs-text'>
+            <h1> SEASIDE SAVORIES </h1>
+
+    <p>Denizden Gelen Lezzetlerin En Güzel Adresi!</p>
+
+    <p>Ege’nin masmavi sularının kenarında yer alan Seaside Savories,
+         size deniz ürünlerinin en tazesini ve en lezizini sunmak için burada.
+          Sıcak bir atmosferde, denizin ferahlatıcı havasını ve eşsiz tatlarını yaşamanız için özenle tasarlanmış restoranımız,
+           gurme deneyimler arayanların adresi.</p>
+    
+    <p>YEMEKLER</p>
+    
+            </div>
+        </div>
+        </div>
+  )
+}
+
+export default Programs
