7c0a38a69afcc2b8d8eb6abc21d21454c24d43f3
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-08-29 23:33:02 +0300

    Update Programs.jsx

--- firstreactproject/src/Programs.jsx
+++ firstreactproject/src/Programs.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import './Programs.css'
 
+
 const Programs = () => {
   return (
     <div className='programs'>
-        <div className="programs">
+        
             <div className='programs-text'>
             <h1> SEASIDE SAVORIES </h1>
 
-    <p>Denizden Gelen Lezzetlerin En Güzel Adresi!</p>
+    <p className="header-text">Denizden Gelen Lezzetlerin En Güzel Adresi!</p>
 
-    <p>Ege’nin masmavi sularının kenarında yer alan Seaside Savories,
+    <h2 className="intro-text">Ege’nin masmavi sularının kenarında yer alan Seaside Savories,
          size deniz ürünlerinin en tazesini ve en lezizini sunmak için burada.
           Sıcak bir atmosferde, denizin ferahlatıcı havasını ve eşsiz tatlarını yaşamanız için özenle tasarlanmış restoranımız,
-           gurme deneyimler arayanların adresi.</p>
-    
-  
-    
+           gurme deneyimler arayanların adresi.</h2>
+
+           <h2 className="gallery-header">GALERİMİZ</h2>
+
+           </div>
+
             </div>
-        </div>
-        </div>
+        
+      
+
   )
 }
 
