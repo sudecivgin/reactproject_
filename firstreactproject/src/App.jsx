b7ada6fc6e7a327f99e194b09c8406baa9eb48ff
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-08-29 23:29:10 +0300

    Update App.jsx

--- firstreactproject/src/App.jsx
+++ firstreactproject/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home/Home'
-
+import Programs from './components/Programs/Programs'
+import Slider from './components/Programs/Slider'
+import Contact from './components/Programs/Contact'
 
 const App = () => {
   return (
       <div className='container'>
       <Navbar/>
       <Home/>
-        <Programs/>
+     <Programs/>
+     <Slider/>
+     <Contact/>
       </div>
   )
 }
