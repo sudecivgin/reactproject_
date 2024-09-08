b4ec1f2fa324593423dcb457358d3b07faf5b125
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-09-09 02:57:29 +0300

    Update App.jsx

--- firstreactproject/src/App.jsx
+++ firstreactproject/src/App.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Home from './components/Home/Home'
-import Programs from './components/Programs/Programs'
-import Slider from './components/Programs/Slider'
-import Contact from './components/Programs/Contact'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home/Home';
+import Hakkımızda from './components/Programs/Hakkımızda';
+import Slider from './components/Programs/Slider';
+import Menü from './components/Programs/Menü';
+import İletişim from './components/Programs/İletişim';
+import { Element } from 'react-scroll';
+import './App.css'; 
 
 const App = () => {
   return (
-      <div className='container'>
-      <Navbar/>
-      <Home/>
-     <Programs/>
-     <Slider/>
-     <Contact/>
-      </div>
-  )
+    <div className='container'>
+      <Navbar />
+
+      <Element name='home'>
+        <Home />
+      </Element>
+
+      <Element name='hakkımızda'>
+        <Hakkımızda />
+      </Element>
+
+      <Element name='slider'>
+        <Slider />
+      </Element>
+
+      <Element name='menu'>
+        <Menü />
+      </Element>
+
+
+      <Element name='iletişim'>
+        <İletişim/>
+      </Element>
+
+
+    </div>
+  );
 }
 
-export default App
+export default App;
