7be7a647494b77566ed90339aad5300aeb3bd971
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-09-12 16:42:18 +0300

    Update Menü.jsx

--- firstreactproject/src/Menü.jsx
+++ firstreactproject/src/Menü.jsx
@@ -1,14 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Menü.css';
 
 
-import ahtapotIzgaraImg from '../../assets/ahtapot2.png';
-import ızgaraBalıkImg from '../../assets/ızgara balık2.png';
-import somonTavaImg from '../../assets/somon tava2.png';
-import kalamarTavaImg from '../../assets/kalamar tava2.png';
-import midyeTavaImg from '../../assets/midye tava2.png';
-import karidesGüveçImg from '../../assets/karides güveç2.png';
-import kalkanFiletoImg from '../../assets/kalkan fileto2.png';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const images = document.querySelectorAll('.menu-item-img');
+  const overlay = document.createElement('div');
+  overlay.className = 'image-overlay';
+  const overlayImg = document.createElement('img');
+  overlay.appendChild(overlayImg);
+  document.body.appendChild(overlay);
+
+
+
+  images.forEach(img => {
+    img.addEventListener('click', (e) => {
+      overlayImg.src = e.target.src; 
+      overlay.classList.add('show'); 
+    });
+  });
+
+  overlay.addEventListener('click', () => {
+    overlay.classList.remove('show'); 
+  });
+});
+
+
+import imagess from '../../assets/menu-son2.png';
+
+import saatImg from '../../assets/saat4.png'; 
+
+
 
 import kalamarDolmaImg from '../../assets/kalamar_dolma.png';
 import giritYahnisiImg from '../../assets/Girit Yahnisi.png';
@@ -17,152 +39,105 @@ import patatesKoftesiImg from '../../assets/patates_koftesi.png';
 import tereyagindaKaridesImg from '../../assets/tereyaginda_karides.png';
 import denizMahsulleriGuvecImg from '../../assets/deniz_mahsulleri_guvec.png';
 
+import ahtapotIzgaraImg from '../../assets/ahtapot2.png';
+import karidesGuvecImg from '../../assets/karides güveç2.png';
+import kalamarTavaImg from '../../assets/kalamar tava2.png';
+import kalkanFiletoImg from '../../assets/kalkan fileto2.png';
+import somonTavaImg from '../../assets/somon tava2.png';
+import izgaraBalikImg from '../../assets/ızgara balık2.png';
+import midyeTavaImg from '../../assets/midye tava2.png';
 
 import suImg from '../../assets/su.png';
 import salgamImg from '../../assets/şalgam.png';
-import beyazsarapImg from '../../assets/beyaz şarap.png';
-import kirmizisarapImg from '../../assets/kırmızı şarap.png';
+import beyazSarapImg from '../../assets/beyaz şarap.png';
+import kirmiziSarapImg from '../../assets/kırmızı şarap.png';
 import mojitoImg from '../../assets/mojito.png';
-
-
-
-
+import narenciyeImg from '../../assets/narenciye.png';
 
 const Menü = () => {
-  const [selectedItem, setSelectedItem] = useState(null);
-
-  const handleClick = (item) => {
-    setSelectedItem(item);
-  };
-
-  const closeImage = () => {
-    setSelectedItem(null);
-  };
-
-  const getImageSrc = () => {
-    switch (selectedItem) {
-      case 'Ahtapot Izgara':
-        return ahtapotIzgaraImg;
-      case 'Somon Tava':
-        return somonTavaImg;
-      case 'Izgara Balık':
-        return ızgaraBalıkImg;
-      case 'Kalamar Tava':
-        return kalamarTavaImg;
-      case 'Midye Tava':
-        return midyeTavaImg;
-      case 'Karides Güveç':
-        return karidesGüveçImg;
-      case 'Kalkan Fileto':
-        return kalkanFiletoImg;
-      case 'Kalamar Dolma':
-        return kalamarDolmaImg;
-      case 'Girit Yahnisi':
-        return giritYahnisiImg;
-      case 'Jumbo Karides':
-        return jumboKaridesImg;
-      case 'Patates Köftesi':
-        return patatesKoftesiImg;
-      case 'Tereyağında Karides':
-        return tereyagindaKaridesImg;
-      case 'Deniz Mahsülleri Güveç':
-        return denizMahsulleriGuvecImg;
-
-
-        case 'Su':
-          return suImg;
-          
-      case 'Şalgam':
-        return salgamImg;
-        
-      case 'Beyaz Şarap':
-        return beyazsarapImg;
-        
-      case 'Kırmızı Şarap':
-        return kirmizisarapImg;
-        
-      case 'Mojito':
-        return mojitoImg;
-
-
-      default:
-        return null;
-    }
+  const menuItems = {
+    'Kalamar Dolma': kalamarDolmaImg,
+    'Girit Yahnisi': giritYahnisiImg,
+    'Jumbo Karides': jumboKaridesImg,
+    'Patates Köftesi': patatesKoftesiImg,
+    'Tereyağında Karides': tereyagindaKaridesImg,
+    'Deniz Mahsülleri Güveç': denizMahsulleriGuvecImg,
+    'Ahtapot Izgara': ahtapotIzgaraImg,
+    'Karides Güveç': karidesGuvecImg,
+    'Kalamar Tava': kalamarTavaImg,
+    'Kalkan Fileto': kalkanFiletoImg,
+    'Somon Tava': somonTavaImg,
+    'Izgara Balık': izgaraBalikImg,
+    'Midye Tava': midyeTavaImg,
+    'Su': suImg,
+    'Şalgam': salgamImg,
+    'Beyaz Şarap': beyazSarapImg,
+    'Kırmızı Şarap': kirmiziSarapImg,
+    'Mojito': mojitoImg,
+    'Narenciye Suları':narenciyeImg
   };
 
   return (
     <div className='menu'>
-            <div className='menu-headers'>
-            <div className='menu-sections-container'> 
-               <div className='menu-section'>
-
-      <div className='menu-text'>
-
-
-  <h1>MENÜ</h1>
-
-
-  <p className='subtitle'>ARA SICAKLAR</p>
-  <div className='arasicak-buttons'>
-    <li onClick={() => handleClick('Kalamar Dolma')}>Kalamar Dolma</li>
-    <li onClick={() => handleClick('Girit Yahnisi')}>Girit Yahnisi</li>
-    <li onClick={() => handleClick('Jumbo Karides')}>Jumbo Karides</li>
-    <li onClick={() => handleClick('Patates Köftesi')}>Patates Köftesi</li>
-    <li onClick={() => handleClick('Tereyağında Karides')}>Tereyağında Karides</li>
-    <li onClick={() => handleClick('Deniz Mahsülleri Güveç')}>Deniz Mahsülleri Güveç</li>
-  </div>
-
-
-  {selectedItem && (
-    <div className='image-overlay' onClick={closeImage}>
-      <div className='image-container'>
-        <img src={getImageSrc()} alt={selectedItem} />
-      </div>
-    </div>
-    
-
-  )}
-</div>
-</div>
-</div>
+      <div className='menu-headers'>
+        <div className='menu-sections-container'>
+          <div className='menu-section'>
+            <div className='menu-text'>
+              <h1>MENÜ</h1>
+              <div className='imagess'>
+                <img src={imagess} alt='menuu.png' className='imagess' />
+              </div>
 
 
 
-<div className='text'>
-        <p className='text-1'>ANA YEMEKLER</p>
-        <ul className='menu-items'>
-          <li onClick={() => handleClick('Ahtapot Izgara')}>Ahtapot Izgara</li>
-          <li onClick={() => handleClick('Karides Güveç')}>Karides Güveç</li>
-          <li onClick={() => handleClick('Kalamar Tava')}>Kalamar Tava </li>
-          <li onClick={() => handleClick('Kalkan Fileto')}>Kalkan Fileto</li>
-          <li onClick={() => handleClick('Somon Tava')}>Somon Tava</li>
-          <li onClick={() => handleClick('Izgara Balık')}>Izgara Balık</li>
-          <li onClick={() => handleClick('Midye Tava')}>Midye Tava</li>
-        </ul>
+              <p className='subtitle'>ARA SICAKLAR</p>
+<ul className='menu-list ara-sicak-list'>
+  {Object.keys(menuItems).slice(0, 6).map(item => (
+    <li key={item} className='ara-sicak-item'>
+      <img src={menuItems[item]} alt={item} className='menu-item-img' />
+      {item}
+    </li>
+  ))}
+</ul>
 
-      </div>
-      <div>
-            <p className='beverages-title'>İÇECEKLER</p>
-            <ul className='beverages-buttons'>
-
-              <li onClick={() => handleClick('Su')}>Su</li>
-              <li onClick={() => handleClick('Şalgam')}>Şalgam</li>
-              <li onClick={() => handleClick('Beyaz Şarap')}>Beyaz Şarap</li>
-              <li onClick={() => handleClick('Kırmızı Şarap')}>Kırmızı Şarap</li>
-
-              <li onClick={() => handleClick('Mojito')}>Mojito</li>
-
-
-            </ul>
+            </div>
           </div>
+        </div>
 
+        <div className='text'>
+          <p className='text-1'>ANA YEMEKLER</p>
+          <ul className='menu-list'>
+            {Object.keys(menuItems).slice(6, 13).map(item => (
+              <li key={item}>
+                <img src={menuItems[item]} alt={item} className='menu-item-img' />
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+        <p className='beverages-title'>İÇECEKLER</p>
+          <ul className='menu-list beverages-list'>
+            {Object.keys(menuItems).slice(13).map(item => (
+              <li key={item}>
+                <img src={menuItems[item]} alt={item} className='menu-item-img' />
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className='footer-container'>
+      <div className='footer-image'>
+        <img src={saatImg} alt='footer' className='footer-img' />
+       </div>
+</div>
 
     </div>
-    </div>
-
-
-
   );
 }
+
 
 export default Menü;
