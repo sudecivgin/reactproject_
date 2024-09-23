88e36d4a35dc32a0e09fb0f0dfad3d3c01e1fab1
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-09-24 02:26:32 +0300

    Update Menü.jsx

--- firstreactproject/src/Menü.jsx
+++ firstreactproject/src/Menü.jsx
@@ -1,35 +1,9 @@
 import React from 'react';
 import './Menü.css';
+import { Container, Row, Col } from 'react-bootstrap'; 
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const images = document.querySelectorAll('.menu-item-img');
-  const overlay = document.createElement('div');
-  overlay.className = 'image-overlay';
-  const overlayImg = document.createElement('img');
-  overlay.appendChild(overlayImg);
-  document.body.appendChild(overlay);
-
-
-
-  images.forEach(img => {
-    img.addEventListener('click', (e) => {
-      overlayImg.src = e.target.src; 
-      overlay.classList.add('show'); 
-    });
-  });
-
-  overlay.addEventListener('click', () => {
-    overlay.classList.remove('show'); 
-  });
-});
-
-
-import imagess from '../../assets/menu-son2.png';
-
-import saatImg from '../../assets/saat4.png'; 
-
+import imagess from '../../assets/menu.png';
+import saatImg from '../../assets/saat4.png';
 
 
 import kalamarDolmaImg from '../../assets/kalamar_dolma.png';
@@ -38,7 +12,6 @@ import jumboKaridesImg from '../../assets/jumbo_karides.png';
 import patatesKoftesiImg from '../../assets/patates_koftesi.png';
 import tereyagindaKaridesImg from '../../assets/tereyaginda_karides.png';
 import denizMahsulleriGuvecImg from '../../assets/deniz_mahsulleri_guvec.png';
-
 import ahtapotIzgaraImg from '../../assets/ahtapot2.png';
 import karidesGuvecImg from '../../assets/karides güveç2.png';
 import kalamarTavaImg from '../../assets/kalamar tava2.png';
@@ -46,7 +19,6 @@ import kalkanFiletoImg from '../../assets/kalkan fileto2.png';
 import somonTavaImg from '../../assets/somon tava2.png';
 import izgaraBalikImg from '../../assets/ızgara balık2.png';
 import midyeTavaImg from '../../assets/midye tava2.png';
-
 import suImg from '../../assets/su.png';
 import salgamImg from '../../assets/şalgam.png';
 import beyazSarapImg from '../../assets/beyaz şarap.png';
@@ -74,70 +46,71 @@ const Menü = () => {
     'Beyaz Şarap': beyazSarapImg,
     'Kırmızı Şarap': kirmiziSarapImg,
     'Mojito': mojitoImg,
-    'Narenciye Suları':narenciyeImg
+    'Narenciye Suları': narenciyeImg
   };
 
   return (
-    <div className='menu'>
-      <div className='menu-headers'>
-        <div className='menu-sections-container'>
-          <div className='menu-section'>
-            <div className='menu-text'>
-              <h1>MENÜ</h1>
-              <div className='imagess'>
-                <img src={imagess} alt='menuu.png' className='imagess' />
-              </div>
-
-
-
-              <p className='subtitle'>ARA SICAKLAR</p>
-<ul className='menu-list ara-sicak-list'>
-  {Object.keys(menuItems).slice(0, 6).map(item => (
-    <li key={item} className='ara-sicak-item'>
-      <img src={menuItems[item]} alt={item} className='menu-item-img' />
-      {item}
-    </li>
-  ))}
-</ul>
-
-            </div>
+    <Container fluid className='menu'>
+      
+      <Row className='menu-headers'>
+        <Col>
+          <h1>MENÜ</h1>
+          <div className='imagess'>
+            <img src={imagess} alt='Menu' className='imagess' />
           </div>
-        </div>
+        </Col>
+      </Row>
 
-        <div className='text'>
+  
+      <Row className='menu-section'>
+      
+
+        <Col md={4}>
+          <p className='subtitle'>ARA SICAKLAR</p>
+          <ul className="menu-list">
+            {Object.keys(menuItems).slice(0, 6).map(item => (
+              <li key={item} className='menu-item'>
+                <img src={menuItems[item]} alt={item} className='menu-item-img' />
+                {item}
+              </li>
+            ))}
+          </ul>
+        </Col>
+
+      
+        <Col md={4}>
           <p className='text-1'>ANA YEMEKLER</p>
-          <ul className='menu-list'>
+          <ul className="menu-list">
             {Object.keys(menuItems).slice(6, 13).map(item => (
-              <li key={item}>
+              <li key={item} className='menu-item'>
                 <img src={menuItems[item]} alt={item} className='menu-item-img' />
                 {item}
               </li>
             ))}
           </ul>
-        </div>
+        </Col>
 
-        <div>
-        <p className='beverages-title'>İÇECEKLER</p>
-          <ul className='menu-list beverages-list'>
+
+        <Col md={4}>
+          <p className='beverages-title'>İÇECEKLER</p>
+          <ul className="menu-list">
             {Object.keys(menuItems).slice(13).map(item => (
-              <li key={item}>
+              <li key={item} className='menu-item'>
                 <img src={menuItems[item]} alt={item} className='menu-item-img' />
                 {item}
               </li>
             ))}
           </ul>
-        </div>
-      </div>
-
-      <div className='footer-container'>
-      <div className='footer-image'>
-        <img src={saatImg} alt='footer' className='footer-img' />
-       </div>
-</div>
-
-    </div>
+        </Col>
+      </Row>
+      
+      <Row className='footer-container'>
+        <Col className='footer-image'>
+          <img src={saatImg} alt='Footer' className='footer-img img-fluid' />
+        </Col>
+      </Row>
+    </Container>
   );
-}
-
+};
 
 export default Menü;
