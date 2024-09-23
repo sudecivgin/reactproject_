ceae5623d2253944dac68f7933ac60814290cd4d
Sude Çıvgın <122885324+sudecivgin@users.noreply.github.com>
2024-09-24 02:25:31 +0300

    Update İletişim.jsx

--- firstreactproject/src/İletişim.jsx
+++ firstreactproject/src/İletişim.jsx
@@ -1,44 +1,35 @@
-import React from 'react'
-import './İletişim.css'
-import { FaInstagram } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-
-
-
+import React from 'react';
+import './İletişim.css';
+import { FaInstagram, FaWhatsapp, FaFacebook } from "react-icons/fa";
+import { Container, Row, Col } from 'react-bootstrap'; 
 
 const İletişim = () => {
   return (
-    <div className='contact'>
-<div className="contact-col">
+    <Container fluid className='contact'>
+      <Row className='justify-content-center'>
+        <Col xs={12} md={6} className='contact-col'>
+          <h1>REZERVASYON VE İLETİŞİM</h1>
+          <ul>
+            <li>seasidesavories@gmail.com</li>
+            <li>+90 000 000 00 00</li>
+            <li>Serin Bahçe Caddesi 21.Sokak No:89, İstanbul</li>
+          </ul>
+          <div className='social-media'>
+            <a href='' target='_blank' rel='noopener noreferrer'>
+              <FaInstagram className='social-icon' />
+            </a>
+            <a href='' target='_blank' rel='noopener noreferrer'>
+              <FaWhatsapp className='social-icon' />
+            </a>
+            <a href='' target='_blank' rel='noopener noreferrer'>
+              <FaFacebook className='social-icon' />
+            </a>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-  <h1>REZERVASYON VE İLETİŞİM </h1>
+export default İletişim;
 
-  
-<ul>
-    <li>seasidesavories@gmail.com</li>
-    <li>+90 000 000 00 00</li>
-    <li>Serin Bahçe Caddesi 21.Sokak No:89, İstanbul</li>
-
-</ul>
-
-
-<div className='social-media'>
-          <a href='' target='_blank' rel='noopener noreferrer'>
-            <FaInstagram className='social-icon' />
-          </a>
-          <a href='' target='_blank' rel='noopener noreferrer'>
-            <FaWhatsapp className='social-icon' />
-          </a>
-          <a href='' target='_blank' rel='noopener noreferrer'>
-            <FaFacebook className='social-icon' />
-          </a>
-</div>
-
-   </div>
-   </div>
-
-  )
-}
-
-export default İletişim
